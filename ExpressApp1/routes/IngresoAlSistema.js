fc61490de691d842bd8dd5c59306de9481aee2ab
SebasRios34 <ligasebasrios@gmail.com>
2020-07-17 22:21:27 -0600

    Agregar usuario --> IngresoAlSistema

--- ExpressApp1/routes/IngresoAlSistema.js
+++ ExpressApp1/routes/IngresoAlSistema.js
@@ -20,8 +20,63 @@ router.post('/confirmarCredenciales', function (req, res, next) {
             res.render('iniciosesion', recordset);
         }
     });
+});
 
+router.post('/agregarUsuario', function (req, res, next) {
+    var nombre = req.body.nombre;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password1 = req.body.password1;
+    var preguntaSeguridad = req.body.preguntaSeguridad;
+    var repuestaSeguridad = req.body.repuestaSeguridad;
+    var rol = req.body.rol;
 
+    db.query('select COUNT(*) as respuesta from tbUsuarios', function (error, recordset) {
+        if (error) {
+            console.log("wrong");
+            return;
+        } else if (recordset.recordset[0]['respuesta'] > 0) {
+            console.log("insertando usuario...");
+            var numId = recordset.recordset[0]['respuesta'];
+            var usuarioId = numId + 1;
+            var id = 'USR-' + usuarioId;
+
+            db.query('select COUNT(*) as pregunta from tbPreguntasSeguridad', function (error, recordset) {
+                if (error) {
+                    console.log("wrong");
+                    return;
+                } else {
+                    console.log("insertando pregunta...");
+                    var numId1 = recordset.recordset[0]['pregunta'];
+                    var preguntaId = numId1 + 1;
+                    var id1 = 'P-' + preguntaId;
+                    db.query("EXEC agregarPregunta @idPregunta = '" + id1 + "', @pregunta = '" + preguntaSeguridad + "'", function (error, rows) {
+                        if (error) {
+                            console.error(error);
+                            res.render('iniciosesion', { mensaje: 'Error al agregar la información' });
+                        } else {
+                            console.table(rows.recordset);
+                            db.query("EXEC IngresarUsuario @idUsuario = '" + id + "', @loginUsuario = '" + nombre + "', @passwordUsuario = '" + password + "', @email = '" + email + "', @preguntaSeguridad = '" + id1 + "', @respuestaSeguridad = '" + preguntaSeguridad + "', @estado = 'Activo', @rol = '" + rol + "'", function (error, rows) {
+                                if (error) {
+                                    console.log(error);
+                                    console.log(preguntaSeguridad);
+                                    console.log("Error");
+                                    console.log(usuarioId);
+                                    res.render('iniciosesion', { mensaje: 'Error al agregar la información' });
+                                } else {
+                                    console.table(rows.recordset);
+                                    res.render('iniciosesion', { mensaje: 'Se agregó la información' });
+                                }
+                            });
+                        }
+                    })
+                }
+            })
+        } else {
+            console.log("permisos denegados");
+            console.log(recordset);
+        }
+    });
 });
 
 module.exports = router;
